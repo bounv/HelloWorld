e28bd48715abe61121737c82ffd19bc9adc11062
monnetek <boun@monnetek.com>
2016-11-15 11:03:25 -0600

    Secondy day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Person p = new Person();
-        p.name = "Alice";
-        p.age = 30;
-        p.isAlive = true;
+	    Person.isValidName("Bob Johnson");
+
+        System.out.println(Person.hair);
     }
 }
